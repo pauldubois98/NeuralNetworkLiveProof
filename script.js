19dc9ebf5ec924c0f6bfa1b657b130efc3080430
pauldubois98 <pauldubois98@gmail.com>
2023-01-28 08:39:28 +0100

    draw typical function

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ WIDTH = canvas.width;
 function clear() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+function draw_func() {
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 5;
+    ctx.moveTo(0,500);
+    for (var x = -3; x < WIDTH+3; x++) {
+        ctx.lineTo(x, (HEIGHT*5/6) - ((HEIGHT/4) * Math.sin(x * Math.PI * 2 / WIDTH)) - (x*HEIGHT/WIDTH/1.5));
+    }
+    ctx.stroke();
+}
+
+draw_func();
