7b7382638af60c1fa23127759b5a443a6fca9961
pauldubois98 <pauldubois98@gmail.com>
2023-01-28 10:10:46 +0100

    calculate boxes func

--- math.js
+++ math.js
@@ -1,3 +1,7 @@
+BOXES_X = [];
+BOXES_Y = [];
+EPSILON = 10;
+
 function calculate_max_epsilon(){
     var max_epsilon = 0;
     for(var i = 0; i < FUNCTION_PTS_X.length; i++){
@@ -15,5 +19,31 @@ function change_epsilon(){
         console.log(epsilon, "Epsilon is too small");
     } else{
         console.log(epsilon, "Epsilon is good");
+    }
+}
+
+function calculate_boxes(){
+    BOXES_X = [A];
+    // find index of A in FUNCTION_PTS_X
+    var index_A = 0;
+    for(var i = 0; i < FUNCTION_PTS_X.length; i++){
+        if(FUNCTION_PTS_X[i] == A){
+            index_A = i;
+            break;
+        }
+    }
+    BOXES_Y = [FUNCTION_PTS_Y[index_A]];
+    var current_index = index_A;
+    var current_y = FUNCTION_PTS_Y[index_A]
+    for(current_index = index_A+1; FUNCTION_PTS_X[current_index] <= B; current_index++){
+        if(Math.abs(FUNCTION_PTS_Y[current_index] - current_y) > EPSILON){
+            BOXES_X.push(FUNCTION_PTS_X[current_index-1]);
+            BOXES_Y.push(FUNCTION_PTS_Y[current_index-1]);
+            current_y = FUNCTION_PTS_Y[current_index];
+        }
+    }
+    if (!BOXES_X.includes(B)) {
+        BOXES_X.push(B);
+        BOXES_Y.push(FUNCTION_PTS_Y[current_index-1]);
     }
 }
