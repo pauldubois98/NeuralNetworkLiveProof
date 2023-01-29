5b93ec5028ad909805e7ecf783b6f77c66547a26
pauldubois98 <pauldubois98@gmail.com>
2023-01-29 20:56:17 +0100

    added node connection

--- svg_edit.js
+++ svg_edit.js
@@ -8,4 +8,18 @@ function draw_node(cx, cy, r, color, name){
     architecture.appendChild(node_element);
 }
 
-draw_node(100, 100, 10, "#ff0000", "node_a");
+function connect_nodes(node_a, node_b){
+    var link_element = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+    link_element.setAttribute("id", "network");
+    link_element.setAttribute("d", 
+        "M " + node_a.cx.baseVal.value + 
+        " " + node_a.cy.baseVal.value +
+        " L " + node_b.cx.baseVal.value +
+        " " + node_b.cy.baseVal.value);
+    link_element.style.stroke = "#000000";
+    link_element.style.strokeWidth = "1";
+    // architecture.appendChild(link_element);
+    architecture.insertBefore(link_element, architecture.firstChild);
+    return link_element;
+}
+
