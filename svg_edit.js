02ac909c82f4a0ecf37592c3e7858a1883f96001
pauldubois98 <pauldubois98@gmail.com>
2023-01-29 20:26:10 +0100

    first working svg edit

---
+++ svg_edit.js
@@ -0,0 +1,11 @@
+function draw_node(cx, cy, r, color, name){
+    var node_element = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+    node_element.setAttribute("id", name);
+    node_element.setAttribute("cx", cx);
+    node_element.setAttribute("cy", cy);
+    node_element.setAttribute("r", r);
+    node_element.style.fill = color;
+    architecture.appendChild(node_element);
+}
+
+draw_node(100, 100, 10, "#ff0000", "node_a");
