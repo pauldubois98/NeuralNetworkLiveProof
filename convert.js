28a910c490530d9e644d3a9453da22cdeb4194fc
pauldubois98 <pauldubois98@gmail.com>
2023-01-30 23:22:26 +0100

    advance export

--- convert.js
+++ convert.js
@@ -2,7 +2,70 @@ function download_svg(){
     EXPORT = true;
     ctx = new SVGCanvas(800, 400);
     draw_all()
-    document.getElementById("svg").innerHTML = ctx.svg.htmlElement.innerHTML
+    var svg = document.getElementById("svg");
+    svg.innerHTML = ctx.svg.htmlElement.innerHTML
+    // draw text to svg
+    if(draw_legend_checkbox.checked){
+        // a
+        var text_element = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+        text_element.setAttribute("x", A-4);
+        text_element.setAttribute("y", HEIGHT-8);
+        text_element.style.fill = "grey";
+        text_element.innerHTML = "a";
+        svg.appendChild(text_element);
+        // b
+        var text_element = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+        text_element.setAttribute("x", B-5);
+        text_element.setAttribute("y", HEIGHT-7);
+        text_element.style.fill = "grey";
+        text_element.innerHTML = "b";
+        svg.appendChild(text_element);
+    }
+    if(draw_subintervals_checkbox.checked && draw_boxes_checkbox.checked && BOXES.length > 0){
+        // c_n
+        var n = 0;
+        var last_box_y_max = 0;
+        BOXES.forEach(function (box) {
+            var text_element = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+            text_element.setAttribute("x", box.x1-8);
+            text_element.setAttribute("y", 362);
+            text_element.style = "fill:red;font-size:12px";
+            text_element.innerHTML = "c";
+            svg.appendChild(text_element);
+            var text_element = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+            ctx.font = "15px Arial";
+            text_element.setAttribute("x", box.x1-3);
+            text_element.setAttribute("y", 370);
+            text_element.style = "fill:red;font-size:12px";
+            text_element.innerHTML = n;
+            svg.appendChild(text_element);
+            last_box_y_max = box.y_max;
+            n++;
+        });
+        var text_element = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+        text_element.setAttribute("x", B-8);
+        text_element.setAttribute("y", 362);
+        text_element.style = "fill:red;font-size:12px";
+        text_element.innerHTML = "c";
+        svg.appendChild(text_element);
+        var text_element = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+        ctx.font = "15px Arial";
+        text_element.setAttribute("x", B-3);
+        text_element.setAttribute("y", 370);
+        text_element.style = "fill:red;font-size:12px";
+        text_element.innerHTML = n;
+        svg.appendChild(text_element);
+        // dot the lines
+        var dots = undefined
+        for(var i=0; i<svg.children.length; i++){
+            if(svg.children[i].getAttribute("stroke")=="#ff0000"){
+                dots = svg.children[i];
+            }
+        }
+        if(dots){
+            dots.style="stroke-dasharray:4,1;stroke-dashoffset:0"
+        }
+    }
     // download the svg
     var svg_inner = ctx.svg.htmlElement.innerHTML;
     //get svg element
