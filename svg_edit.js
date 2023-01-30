d0187832cad05723fbdf79b4e324d7499cbb3db9
pauldubois98 <pauldubois98@gmail.com>
2023-01-31 00:51:08 +0100

    dropped unused functions

--- svg_edit.js
+++ svg_edit.js
@@ -60,33 +60,6 @@ function apply_network(x){
     draw_network_bis();
 }
 
-function draw_node(cx, cy, r, color, name){
-    var node_element = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
-    node_element.setAttribute("id", name);
-    node_element.setAttribute("cx", cx);
-    node_element.setAttribute("cy", cy);
-    node_element.setAttribute("r", r);
-    node_element.style.fill = color;
-    architecture.appendChild(node_element);
-    return node_element;
-}
-
-function connect_nodes(node_a, node_b){
-    var link_element = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-    link_element.setAttribute("id", "network");
-    link_element.setAttribute("d", 
-        "M " + node_a.cx.baseVal.value + 
-        " " + node_a.cy.baseVal.value +
-        " L " + node_b.cx.baseVal.value +
-        " " + node_b.cy.baseVal.value);
-    link_element.style.stroke = "#000000";
-    link_element.style.strokeWidth = "1";
-    // architecture.appendChild(link_element);
-    architecture.insertBefore(link_element, architecture.firstChild);
-    return link_element;
-}
-
-
 function draw_network_bis(){
     // canvas part
     ctx_ter.clearRect(0, 0, canvas_ter.width, canvas_ter.height);
