bbfc76e9de50e9e54fbb7c25369f824dcc1d165e
pauldubois98 <pauldubois98@gmail.com>
2023-01-31 01:40:03 +0100

    cleaned up dl svg

--- convert.js
+++ convert.js
@@ -1,9 +1,9 @@
-function download_svg(){
+function download_svg(dl=true){
     EXPORT = true;
     ctx = new SVGCanvas(800, 400);
-    draw_all()
+    draw_all();
     var svg = document.getElementById("svg");
-    svg.innerHTML = ctx.svg.htmlElement.innerHTML
+    svg.innerHTML = ctx.svg.htmlElement.innerHTML;
     // draw text to svg
     if(draw_legend_checkbox.checked){
         // a
@@ -66,31 +66,30 @@ function download_svg(){
             dots.style="stroke-dasharray:4,1;stroke-dashoffset:0"
         }
     }
+
     // download the svg
-    var svg_inner = ctx.svg.htmlElement.innerHTML;
-    //get svg element
-    var svg = document.getElementById("svg");
-    //get svg source.
-    var serializer = new XMLSerializer();
-    var source = serializer.serializeToString(svg);
-    //add name spaces.
-    if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
-        source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+    if(dl){
+        //get svg source.
+        var serializer = new XMLSerializer();
+        var source = serializer.serializeToString(svg);
+        //add name spaces.
+        if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
+            source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+        }
+        if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
+            source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+        }
+        // add xml declaration
+        source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+        // convert svg source to URI data scheme.
+        var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
+        // set url value to a element's href attribute.
+        var a = document.createElement("a");
+        a.href = url;
+        a.download = "plot.svg";
+        a.click();
+        window.URL.revokeObjectURL(url);
     }
-    if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
-        source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
-    }
-    // add xml declaration
-    source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
-    // convert svg source to URI data scheme.
-    var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
-
-    var a = document.createElement("a");
-    a.href = url;
-    a.download = "plot.svg";
-    a.click();
-    window.URL.revokeObjectURL(url);
-
     ctx = canvas.getContext('2d');
     EXPORT = false;
 }
